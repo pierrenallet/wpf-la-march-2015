e4dc3fcce3c2f8adba1a8d47b3daa8bd83a32e57
Pierre Nallet <pierrenallet@hotmail.com>
2015-03-20 09:51:19 -0700

    list box demo 1

---
+++ TestSuggestion/TestSuggestion/RichTextBoxProperties.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace TestSuggestion
+{
+    class RichTextBoxProperties
+    {
+
+
+        public static bool GetIsSelectedTextEnabled(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsSelectedTextEnabledProperty);
+        }
+
+        public static void SetIsSelectedTextEnabled(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsSelectedTextEnabledProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for IsSelectedTextEnabled.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsSelectedTextEnabledProperty =
+            DependencyProperty.RegisterAttached("IsSelectedTextEnabled", typeof(bool), typeof(RichTextBoxProperties), new PropertyMetadata(false, OnIsSelectedTextEnabled));
+        private static void OnIsSelectedTextEnabled(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            bool v = (bool)args.NewValue;
+            if (v)
+            {
+                RichTextBox rtb = (RichTextBox)sender;
+                rtb.SelectionChanged += Rtb_SelectionChanged;
+            }
+        }
+
+        private static void Rtb_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            RichTextBox rtb = (RichTextBox)sender;
+            var position = rtb.Selection.End.GetCharacterRect(LogicalDirection.Forward);
+            Debug.WriteLine(" " + position.Right + " " + position.Bottom);
+            ToolTipService.SetVerticalOffset(rtb, position.Bottom);
+            ToolTipService.SetHorizontalOffset(rtb, position.Right);
+            rtb.SetValue(RichTextBoxProperties.SelectedTextProperty, rtb.Selection.Text);
+        }
+
+
+
+        public static string GetSelectedText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(SelectedTextProperty);
+        }
+
+        public static void SetSelectedText(DependencyObject obj, string value)
+        {
+            obj.SetValue(SelectedTextProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedTextProperty =
+            DependencyProperty.RegisterAttached("SelectedText", typeof(string), typeof(RichTextBoxProperties), new PropertyMetadata(null));
+
+
+    }
+}
---
+++ TestSuggestion/TestSuggestion/SuggestionViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSuggestion
+{
+    class SuggestionViewModel : ViewModel
+    {
+        private string _text = string.Empty;
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value;
+                NotifyPropertyChanged();
+                NotifyPropertyChanged(nameof(Suggestions));
+            }
+        }
+        static string[] allSuggestions = new string[] { "Apple", "Orange", "Pear" };
+
+        public IEnumerable<string> Suggestions
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.Text))
+                    return Enumerable.Empty<string>();
+                return allSuggestions.Where(s => s.ToLower().Contains((this.Text??string.Empty).ToLower()));
+
+            }
+        }
+    }
+}
